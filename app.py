c7a0374183029ab5d393d0089688beef68e9b73f
PRAVIN BOKARE <pravinbokare7@gmail.com>
2023-07-01 15:12:24 +0530

    web app completed

--- app.py
+++ app.py
@@ -1,66 +1,38 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import numpy as np
-import yaml
-from from_root import from_root
-import joblib
-#from prediction_service import prediction
+from prediction_service import prediction
+from prediction_service.prediction import NotInRange
 
-params_path = 'params.yaml'
+
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
-app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
-
-def read_params(config_path):
-    config_path = os.path.join(from_root(), config_path)
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config['webapp_model_dir']
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction[0]
-
-
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = {'response':response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {'error': 'Something went wrong'}
-        return render_template('404.html', error=error)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         try:
             if request.form:
-                data = dict(request.form).values()
-                data = [list(map(float, data))]
-                response = predict(data)
-                return render_template('index.html', response=response)
-
+                dict_req = dict(request.form)
+                response = prediction.form_response(dict_req)
+                return render_template("index.html", response=response)
             elif request.json:
-                response = api_response(request)
+                response = prediction.api_response(request.json)
                 return jsonify(response)
-
+        except NotInRange as e:
+            error = {"error": str(e)}
+            return render_template("404.html", error=error)
         except Exception as e:
-            print(e)
-            error = {'error': 'Something went wrong'}
-
-            return render_template('404.html', error=error)
+            error = {"error": "Something went wrong!! Try again later!"}
+            return render_template("404.html", error=error)
     else:
         return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
